960a5d8bf839e9f43a4acd462491dfb1ede59133
Denis Vakulenko <woof88@yandex.ru>
2022-08-23 09:54:39 +0500

    fix: preload movielist

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -6,13 +6,14 @@ import {Search} from '../components/Search'
 class Main extends React.Component {
   state = {
     movies: [],
+    loading: true
   }
 
-  // componentDidMount() {
-  //   fetch('http://img.omdbapi.com/?apikey=6a9aae1a&s=matrix')
-  //     .then(res => res.json())
-  //     .then(data => this.setState({movies: data}))
-  // }
+  componentDidMount() {
+    fetch('http://www.omdbapi.com/?apikey=6a9aae1a&s=terminator')
+      .then(res => res.json())
+      .then(data => this.setState({movies: data.Search}))
+  }
 
   searchMovie = (dataSearch, type = 'all') => {
     fetch(`http://www.omdbapi.com/?apikey=6a9aae1a&s=${dataSearch}${type !== 'all' ? `&type=${type}` : ''}`)
@@ -21,14 +22,18 @@ class Main extends React.Component {
   }
 
   render() {
-    const {movies} = this.state
+    const {movies} = this.state;
 
-    return <main className="container content">
-      <Search searchMovie={this.searchMovie}/>
-      {
-        movies.length ? (<MoviesList movies={this.state.movies}/>) : <Preloader/>
-      }     
+    return (
+      <main className="container content">
+        <Search searchMovie={this.searchMovie}/>
+        {movies.length ? (
+          <MoviesList movies={this.state.movies}/>
+          ) : (
+          <Preloader/>
+          )}     
       </main>
+    )
     }
 }
 
